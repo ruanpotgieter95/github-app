1d937009e97b6f68b09767239760ffcb20f7749d
ruanpotgieter95 <ruan@al.co.za>
2020-08-24 21:05:16 +0200

    search repo on mount

--- src/components/RepositoryList/index.js
+++ src/components/RepositoryList/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { useSelector, shallowEqual } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import { RepositoryCard, RepositoryPlaceholder } from 'components';
+
 import './styles.scss';
+import { searchRepositories } from 'api';
 
 export default () => {
+    const dispatch = useDispatch();
+    const { search } = useParams();
     const { repositoryList, loading, repositoryTotal } = useSelector(state => ({
         repositoryList: state.repository.list.items,
         repositoryTotal: state.repository.list.total_count,
         loading: state.repository.loading
     }), shallowEqual);
+
+    useEffect(() => {
+        dispatch(searchRepositories(search))
+    },[])
 
     return (
         <div className="repository_list">
