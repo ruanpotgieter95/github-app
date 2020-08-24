0d180aae45be5cc41e692a2da66a22e1e7b86fd3
ruanpotgieter95 <ruan@al.co.za>
2020-08-24 13:41:31 +0200

    logo on navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { login } from 'actions';
+import Logo from 'assets/logo/Octocat.png';
 import './styles.scss';
 
 export default () => {
@@ -11,7 +12,7 @@ export default () => {
     return (
         <nav>
             <div>
-                <span>Logo</span>
+                <img src={Logo} alt="" />
                 <span className="hover button" 
                     onClick={() => history.push('/repositories')}
                 >Repositories</span>
