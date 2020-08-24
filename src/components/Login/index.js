e9168424ae89d655443569d835369fc53d1c30c6
ruanpotgieter95 <ruan@al.co.za>
2020-08-24 21:22:32 +0200

    login page design

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { login } from 'actions';
+import Logo from 'assets/logo/Octocat.png';
 import './styles.scss';
 
 export default () => {
     const dispatch = useDispatch();
+
+    const submit = (event) => {
+        event.preventDefault();
+        dispatch(login(false, true));
+    }
+
     return (
         <div className="login">
-            <button onClick={() => dispatch(login(false, true))}>Login</button>
+            <form onSubmit={submit}>
+                <img src={Logo} alt="" />
+                <h3>Login</h3>
+                <sub>Only used to show private/public routes</sub>
+                <sub>Type a fake email and password</sub>
+                <input type="email" required/>
+                <input type="password" required/>
+                <button>Login</button>
+            </form>
         </div>
     )
 }
