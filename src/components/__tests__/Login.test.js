87a5cd01ca7db74f71c5566506a0046dc7047780
ruanpotgieter95 <ruan@al.co.za>
2020-08-24 21:32:53 +0200

    additional tests for login

--- src/components/__tests__/Login.test.js
+++ src/components/__tests__/Login.test.js
@@ -18,6 +18,22 @@ describe('Login', () => {
         wrapped.unmount();
     });
 
+    it('has text with value "Login"', () => {
+        expect(wrapped.render().text()).toContain('Login');
+    });
+
+    it('has text with value "Only used to show private/public routes"', () => {
+        expect(wrapped.render().text()).toContain('Only used to show private/public routes');
+    });
+
+    it('has text with value "Type a fake email and password"', () => {
+        expect(wrapped.render().text()).toContain('Type a fake email and password');
+    });
+
+    it('has two inputs', () => {
+        expect(wrapped.find('input').length).toEqual(2);
+    });
+
     it('has button', () => {
         expect(wrapped.find('button').length).toEqual(1);
     });
