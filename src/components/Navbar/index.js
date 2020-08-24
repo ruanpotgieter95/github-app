8fab6d1ae17881ee7205cb6f4bd033e5340f48aa
ruanpotgieter95 <ruan@al.co.za>
2020-08-24 08:18:55 +0200

    dashboard navigation

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { login } from 'actions';
 import { SearchBar } from 'components';
@@ -6,9 +7,16 @@ import './styles.scss';
 
 export default () => {
     const dispatch = useDispatch();
+    const history = useHistory();
+
     return (
         <nav>
-            <span>Logo</span>
+            <div>
+                <span>Logo</span>
+                <span className="hover button" 
+                    onClick={() => history.push('/dashboard')}
+                >Dashboard</span>
+            </div>
             <SearchBar />
             <span className="hover" 
                 onClick={() => dispatch(login(false, false))}
