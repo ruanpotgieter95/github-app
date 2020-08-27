6a5defb4da257d0e4ae2edf8ef6d82a622bdd8b1
ruanpotgieter95 <ruan@al.co.za>
2020-08-27 12:01:24 +0200

    Updates

---
+++ precache-manifest.14d390a8bc84fe9ead880ed247b0a561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b57b783f0c4d35b2887149c5af32ab2",
+    "url": "/github-app/index.html"
+  },
+  {
+    "revision": "50398a3d21cb49293249",
+    "url": "/github-app/static/css/main.089037af.chunk.css"
+  },
+  {
+    "revision": "0f7696c7eb2245f61edb",
+    "url": "/github-app/static/js/2.19330777.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/github-app/static/js/2.19330777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50398a3d21cb49293249",
+    "url": "/github-app/static/js/main.88772f04.chunk.js"
+  },
+  {
+    "revision": "1c70619a28ceb182c4ef",
+    "url": "/github-app/static/js/runtime-main.58f17b4a.js"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/github-app/static/media/Octocat.0ba2aa20.png"
+  }
+]);
